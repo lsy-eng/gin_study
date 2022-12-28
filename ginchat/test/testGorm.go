9a14a3b2a5819caff4e090bf5b5ef8985412d475
lsy <550489349@qq.com>
2022-12-28 12:41:00 +0800

    ginchat mysql creat and connect

--- ginchat/test/testGorm.go
+++ ginchat/test/testGorm.go
@@ -1,8 +1,11 @@
 package main
 
 import (
-  "gorm.io/gorm"
-  "gorm.io/driver/mysql"
+	"fmt"
+	"ginchat/unit3_IM/ginchat/models"
+ 	"gorm.io/gorm"
+ 	"gorm.io/driver/mysql"
+  
 )
 
 type Product struct {
@@ -12,28 +15,33 @@ type Product struct {
 }
 
 func main() {
-  db, err := gorm.Open(mysql.Open("test.db"), &gorm.Config{})
+  db, err := gorm.Open(mysql.Open("root:123456@tcp(127.0.0.1:3306)/ginchat?charset=utf8mb4&parseTime=True&loc=Local"), &gorm.Config{}) //这部分下载了数据库回头看
   if err != nil {
     panic("failed to connect database")
   }
 
   // Migrate the schema
-  db.AutoMigrate(&Product{})
+  db.AutoMigrate(&models.UserBasic{})
 
   // Create
-  db.Create(&Product{Code: "D42", Price: 100})
+  user := &models.UserBasic{}
+  user.Name = "刘诗源"
+  db.Create(user)
+
+  
 
   // Read
-  var product Product
-  db.First(&product, 1) // find product with integer primary key
-  db.First(&product, "code = ?", "D42") // find product with code D42
+  //var product Product
+  fmt.Println(db.First(user, 1))
+  //db.First(user, 1) // find product with integer primary key
+  //db.First(user, "code = ?", "D42") // find product with code D42
 
   // Update - update product's price to 200
-  db.Model(&product).Update("Price", 200)
+  db.Model(user).Update("PassWord", "1234")
   // Update - update multiple fields
-  db.Model(&product).Updates(Product{Price: 200, Code: "F42"}) // non-zero fields
-  db.Model(&product).Updates(map[string]interface{}{"Price": 200, "Code": "F42"})
+  //db.Model(user).Updates(Product{Price: 200, Code: "F42"}) // non-zero fields
+  //db.Model(&product).Updates(map[string]interface{}{"Price": 200, "Code": "F42"})
 
   // Delete - delete product
-  db.Delete(&product, 1)
+  //db.Delete(&product, 1)
 }
